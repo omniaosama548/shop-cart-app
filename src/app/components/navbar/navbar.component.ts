3a76f1d8a42e3c392fc0d5cad939a0568a609ce7
omnia <anaomnia47@gmail.com>
2025-08-11 18:25:43 +0300

    handle

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+// @ts-ignore
+import 'bootstrap';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+
 import { AuthService } from '../../services/auth.service';
 import { CartService } from '../../services/cart.service';
-
+declare var bootstrap: any;
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit ,AfterViewInit{
+ngAfterViewInit() {
+  // نجيب كل الروابط اللي جوه القائمة
+  const navLinks = document.querySelectorAll('#mainNavbar .nav-link');
+
+  // نجيب عنصر القائمة نفسها
+  const navbarCollapse = document.getElementById('mainNavbar');
+
+  if (navbarCollapse) {
+    const bsCollapse = new bootstrap.Collapse(navbarCollapse, { toggle: false });
+
+    navLinks.forEach(link => {
+      link.addEventListener('click', () => {
+        bsCollapse.hide(); // قفل القائمة عند الضغط على أي لينك
+      });
+    });
+  }
+}
+
   numOfCartItems:number=0;
   constructor(private auth:AuthService,private cartService:CartService) { }
   isLoggedIn: boolean = false;
